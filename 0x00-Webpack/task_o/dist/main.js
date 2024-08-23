c96ea554d2f0b6ab69a13c1f1836bae4e61e83eb
Mubby2003 <adetonamubarak12@gmail.com>
2024-08-24 00:48:18 +0100

    good

---
+++ 0x00-Webpack/task_o/dist/main.js
@@ -0,0 +1,32 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/index.js":
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/***/ (() => {
+
+eval("throw new Error(\"Module build failed (from ./node_modules/babel-loader/lib/index.js):\\nError: Cannot find module 'fs/promises'\\nRequire stack:\\n- /root/alx-react/0x00-Webpack/task_o/node_modules/babel-loader/lib/cache.js\\n- /root/alx-react/0x00-Webpack/task_o/node_modules/babel-loader/lib/index.js\\n- /root/alx-react/0x00-Webpack/task_o/node_modules/loader-runner/lib/loadLoader.js\\n- /root/alx-react/0x00-Webpack/task_o/node_modules/loader-runner/lib/LoaderRunner.js\\n- /root/alx-react/0x00-Webpack/task_o/node_modules/webpack/lib/NormalModuleFactory.js\\n- /root/alx-react/0x00-Webpack/task_o/node_modules/webpack/lib/Compiler.js\\n- /root/alx-react/0x00-Webpack/task_o/node_modules/webpack/lib/webpack.js\\n- /root/alx-react/0x00-Webpack/task_o/node_modules/webpack/lib/index.js\\n- /root/alx-react/0x00-Webpack/task_o/node_modules/webpack-cli/lib/webpack-cli.js\\n- /root/alx-react/0x00-Webpack/task_o/node_modules/webpack-cli/lib/bootstrap.js\\n- /root/alx-react/0x00-Webpack/task_o/node_modules/webpack-cli/bin/cli.js\\n- /root/alx-react/0x00-Webpack/task_o/node_modules/webpack/bin/webpack.js\\n    at Function.Module._resolveFilename (internal/modules/cjs/loader.js:815:15)\\n    at Function.Module._load (internal/modules/cjs/loader.js:667:27)\\n    at Module.require (internal/modules/cjs/loader.js:887:19)\\n    at require (internal/modules/cjs/helpers.js:74:18)\\n    at Object.<anonymous> (/root/alx-react/0x00-Webpack/task_o/node_modules/babel-loader/lib/cache.js:21:5)\\n    at Module._compile (internal/modules/cjs/loader.js:999:30)\\n    at Object.Module._extensions..js (internal/modules/cjs/loader.js:1027:10)\\n    at Module.load (internal/modules/cjs/loader.js:863:32)\\n    at Function.Module._load (internal/modules/cjs/loader.js:708:14)\\n    at Module.require (internal/modules/cjs/loader.js:887:19)\");\n\n//# sourceURL=webpack://task_o/./src/index.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module doesn't tell about it's top-level declarations so it can't be inlined
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./src/index.js"]();
+/******/ 	
+/******/ })()
+;
