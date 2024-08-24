a3ae0a2b4e6d943b6fffc5281bd704b7988ae402
Mubby2003 <adetonamubarak12@gmail.com>
2024-08-24 01:03:22 +0100

    dd

--- 0x00-Webpack/task_o/webpack.config.js
+++ 0x00-Webpack/task_o/webpack.config.js
@@ -3,15 +3,6 @@ const path = require("path");
 const config = {
   entry: "./src/index.js",
   mode: "development",
-  module: {
-    rules: [
-      {
-        exclude: /(node_modules)/,
-        test: /\.(js|jsx)$/i,
-        loader: "babel-loader"
-      }
-    ]
-  },
   output: {
     path: path.resolve(__dirname, "dist")
   },
